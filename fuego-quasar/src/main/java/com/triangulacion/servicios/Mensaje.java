7c8d7910c48b5f3917fdc07aa91e758a1b825986
matby.peralta <matbyperalta@hotmail.com>
2020-11-06 18:19:52 -0500

    ajustes de versionamiento

--- fuego-quasar/src/main/java/com/triangulacion/servicios/Mensaje.java
+++
@@ -1,9 +0,0 @@
-package com.triangulacion.servicios;
-
-public interface Mensaje {
-
-	public double [] getLocation(double ...distances) throws Exception;
-	
-	public String getMessage(String []... msg) throws Exception;
-	
-}
